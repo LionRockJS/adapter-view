7c1686f87d0e2092f33b9cef6a82da9a7aa2b8fa
Colin Leung <colin@cowise.co>
2024-05-14 17:01:23 +0800

    fix: filter not loaded

--- init.mjs
+++ init.mjs
@@ -2,5 +2,5 @@ import { Central } from '@lionrockjs/central';
 import config from './config/liquidjs.mjs';
 
 await Central.initConfig(new Map([
-  ['liquid', config],
+  ['liquidjs', config],
 ]));
